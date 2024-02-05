24a6ab19d675f28a573030fa52983bf8342f44b4
Michael Fehrenbach <mf@michaelfehrenbach.com>
2024-02-04 19:48:01 -0500

    `renderUser` func

--- assets/arena.js
+++ assets/arena.js
@@ -1,5 +1,5 @@
 // This allows us to process/render the descriptions, which are in Markdown!
-// More about Markdown: https://www.markdownguide.org
+// More about Markdown: https://en.wikipedia.org/wiki/Markdown
 let markdownIt = document.createElement('script')
 markdownIt.src = 'https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js'
 document.head.appendChild(markdownIt)
@@ -128,6 +128,21 @@ const renderBlock = (block) => {
 
 
 
+// It‘s always good to credit your work:
+const renderUser = (user, container) => { // You can have multiple arguments for a function!
+	const userAddress =
+		`
+		<address>
+			<img src="${ user.avatar_image.display }">
+			<h3>${ user.first_name }</h3>
+			<p><a href="https://are.na/${ user.slug }">Are.na profile ↗</a></p>
+		</address>
+		`
+	container.insertAdjacentHTML('beforeend', userAddress)
+}
+
+
+
 // Now that we have said what we can do, go get the data:
 fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-store' })
 	.then((response) => response.json()) // Return it as JSON data
@@ -140,4 +155,9 @@ fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-stor
 			// console.log(block) // The data for a single block
 			renderBlock(block) // Pass the single block data to the render function
 		})
+
+		// Also display the owner and collaborators:
+		const channelUsers = document.getElementById('channel-users') // Show them together
+		renderUser(data.owner, channelUsers)
+		data.collaborators.forEach((collaborator) => renderUser(collaborator, channelUsers))
 	})
